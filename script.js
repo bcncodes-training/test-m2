d9ce78f34ac60af8bbb382079174f77f02d3da09
naranjito72 <theclick36@gmail.com>
2019-06-13 13:23:14 +0200

    robert exam1 commit

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
 class Song {
-    constructor(){
-    }
+    constructor(name, listeners, url, group){
+        this.group = group;//parametros que necesito sacar de groupName.
+        this.name = name;
+        this.listeners = listeners;
+        this.url = url;//vas bien, es mas fácil de lo que crees
+        
 
-    setItemLi(){
+
     }
+//getElement para .lista
+//addEventListener innerHtml
+
+//elementos llamada html
+    setItemLi(){
+        let list = document.createElement("li");
+        let enlace = document.createElement("a");
+        let box = document.createElement("div");
+        let element1 = document.getElementsByClassName("lista");
+
+        list.appendChild(enlace, box);
+        element1.appendChild(list);
+
+    }
+    
     setItemGroupName(group,url){
+        //sacar de tracks [] 
+        let str = tracks;// me reconoce el array
+       
+        // for( i = 0; i < str.length; i++){//recorre,pero me dice que result ya no es una array
+            // como recorremos objetos?for... in!!
+            for(let name in str){
+             console.log(name + ": " + str[name]);
+             //lee objetos
+            }
+        
+       // console log no me da error pero me dice que no tengo declaradas ninguna de las let
     }
     setItemSongTitle(title){
+        //track[]
     }
     setListeners(listeners){
+        //sacar de setItem
     }
     getNewElement(group,url,title,listeners){
+
     }
 
 }
-
+//pasar array de canciones tracks[]
 const loadSongs = ()=>{
+ 
 
+}
 const loadOverview = () =>{
 }
 
@@ -29,7 +64,8 @@ const loadBiggest = (e)=>{
 }
 
 const init = ()=>{
- 
+ //declaraciones y donde pintaremos finalmente
+
     }
 
 
