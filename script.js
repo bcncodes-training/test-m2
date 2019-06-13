97430923d46ee141022cf125cfc23c1c0eec501b
unknown <you@example.com>
2019-06-13 22:46:39 +0200

    casa commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ class Song {
         this.group = group;//parametros que necesito sacar de groupName.
         this.name = name;
         this.listeners = listeners;
-        this.url = url;//vas bien, es mas fácil de lo que crees
+        this.url = url;
         
 
 
@@ -16,36 +16,45 @@ class Song {
         let list = document.createElement("li");
         let enlace = document.createElement("a");
         let box = document.createElement("div");
-        let element1 = document.getElementsByClassName("lista");
+        let element1 = document.getElementById("lista");
 
-        list.appendChild(enlace, box);
+        list.appendChild(enlace);
+        list.appendChild( box);
         element1.appendChild(list);
-
+        let html = element1.outerHTML;//coge TODO el html
+        console.log(html);
     }
     
     setItemGroupName(group,url){
         //sacar de tracks [] 
-        let str = tracks;// me reconoce el array
-        this.name = name;
+        //let str = tracks;// me reconoce el array
+        //this.name = name;
        
         // for( i = 0; i < str.length; i++){//recorre,pero me dice que result ya no es una array
             // como recorremos objetos?for... in!!
-            for(let name in str){
+          /*  for(let name in str){
              console.log(name + ": " + str[name]);
              //lee objetos
              if (str.hasOwnProperty(name)){
                  group = name;
              }
             // const name = Object.keys(name);
-            }
+            }*/
         
-       // console log no me da error pero me dice que no tengo declaradas ninguna de las let
+       //DE NUEVO
+       let track = tracks.shift();//problema, solo coge la primera!
+       let song = new Song(track.url, track.artist.name);
+       console.log(song);
     }
     setItemSongTitle(title){
         //track[]
+        let track = tracks.shift();
+        let song = new Song(track.name);
     }
     setListeners(listeners){
         //sacar de setItem
+        let track = tracks.shift();
+        let song = new Song(track.listeners);
     }
     getNewElement(group,url,title,listeners){
 
