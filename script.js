8ab8a5bc0ab20fbc833d3d27a88eb8165cc31e32
Francesc Parra <francesc.parra@gmail.com>
2019-06-13 13:49:03 +0200

    primer intento

--- script.js
+++ script.js
@@ -1,37 +1,79 @@
-class Song {
-    constructor(){
+//LÓGICA
+
+
+class song {
+    constructor(group, title, listeners, url){ 
+        this.group = group;
+        this.title = title;
+        this.listeners = listeners;
+        this.url = url;
+        // list="";
     }
 
-    setItemLi(){
-    }
     setItemGroupName(group,url){
-    }
+        return document.createElement(`<li class="far fa-play-circle"><a href="${this.url}" class="group-name">${this.group}: </a>`);        
+        }
     setItemSongTitle(title){
+        return document.createElement(`<a class="song-title>${title}</a>`)
     }
     setListeners(listeners){
-    }
-    getNewElement(group,url,title,listeners){
-    }
-
+        return document.createElement(`<div class="listeners">${listeners}</div>`)
+    }    
+    setItemLi(){        
+        return document.createElement('li');
+    }    
+    getNewElement(group,url,title,listeners){        
+        let list = setItemLi()        
+        list.appendChild(setItemGroupName(group,url));
+        list.appendChild(setItemSongTitle(title));
+        list.appendChild(setListeners(listeners));
+        return list;
+    }        
 }
 
-const loadSongs = ()=>{
-
-const loadOverview = () =>{
+const loadSongs = (tracks)=>{
+    let newArray = [];     
+    for (i=0; i<tracks.length; i++){        
+        let newSong = new Song(tracks[i].artist.name, tracks[i].name, tracks[i].listeners, tracks[i].artist.url);
+        newArray.push(newSong)
+    }
+    let lista = document.getElementsByClassName("lista")
 }
 
-const loadTenListened = ()=>{
+
+
+
+
+
+
+
+
+
+// class Song {
+//     constructor(){
+//     }
+
+
+
+// }
+
+
+
+// const loadOverview = () =>{
+// }
+
+// const loadTenListened = ()=>{
  
-}
+// }
 
-const loadBiggest = (e)=>{
+// const loadBiggest = (e)=>{
    
-}
+// }
 
-const init = ()=>{
+// const init = ()=>{
  
-    }
+//     }
 
 
-window.onload = init;
+// window.onload = init;
 
